90891045851603144bc55b1a45a7408c71d6ebe4
Hira Soomro <hira@Hiras-MacBook-Pro.local>
2023-03-12 19:00:31 -0700

    updated files on the code to change the directories

--- app.js
+++ app.js
@@ -1,184 +1,111 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const html = require('./templates/templates.html');
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "./Output/teams.html");
+const render = require("lib/html.js");
 
+const employeeList = [];
 
+//Develop the code to create a blueprint for the questions and employee type
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter employee name:"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter employee ID number:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter employee e-mail address:"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role in the team?",
+        choices: [{
+            name: "Manager"
+        },
+        {
+            name: "Engineer"
+        },
+        {
+            name: "Intern"
+        }]
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter manager's office number:",
+        when: function(answers){
+            return answers.role === "Manager";
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter engineer's github username:",
+        when: function(answers){
+            return answers.role === "Engineer";
+        }
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the name of the school you're currently attending?",
+        when: function(answers){
+            return answers.role === "Intern";
+        }
+    }
+];
+function promptUser() {
 
-const writeFileAsync = util.promisify(fs.writeFile);
-const appendFileAsync = util.promisify(fs.appendFile);
-
-let teamArray = [];
-let teamstr = ``;
-
-//this calls all the functions in one in order
-async function main() {
-     try {
-          await prompt()
-          // for i to teamArray.length  => 
-
-          for (let i = 0; i < teamArray.length; i++) {
-               //template literal=``
-               teamstr = teamstr + html.generateCard(teamArray[i]);
-          }
-
-          let finalHTML = html.generateHTML(teamstr)
-
-          console.log(teamstr)
-
-          //call generate function to generate the html template literal
-
-          //write file 
-          writeFileAsync("./output/index.html", finalHTML)
-
-
-     } catch (err) {
-          return console.log(err);
-     }
-
+   inquirer.prompt(questions).then(function(response) {
+       if (response.role === "Manager") {
+           const newManager = new Manager(response.name, response.id, response.email, response.officeNumber);
+           employeeList.push(newManager);
+       }
+       else if (response.role === "Engineer") {
+           const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
+           employeeList.push(newEngineer);
+       }
+       else if (response.role === "Intern") {
+           const newIntern = new Intern(response.name, response.id, response.email, response.school);
+           employeeList.push(newIntern);
+       }
+       newEmployee();
+   });
 };
 
-async function prompt() {
-     let responseDone = "";
-     // prompt to collect input and use do while atleast one and do it number of times depending on the while condition
-     do {
-          try {
-               response = await inquirer.prompt([
-
-                    {
-                         type: "input",
-                         name: "name",
-                         message: "What is the employee's name?: "
-                    },
-                    {
-                         type: "input",
-                         name: "id",
-                         message: "Enter the employee's ID: "
-                    },
-                    {
-                         type: "input",
-                         name: "email",
-                         message: "Enter the employee's email address: "
-                    },
-                    {
-                         type: "list",
-                         name: "role",
-                         message: "What what is the employee's role:",
-                         choices: [
-                              "Engineer",
-                              "Intern",
-                              "Manager"
-                         ]
-                    }
-               ]);
-
-               let response2 = ""
-               // if else statement
-
-               if (response.role === "Engineer") {
-                    response2 = await inquirer.prompt([{
-                         type: "input",
-                         name: "x",
-                         message: "What is the employee's github username?:",
-                    }, ]);
-                    //store the object and push
-                    const engineer = new Engineer(response.name, response.id, response.email, response2.x);
-                    teamArray.push(engineer);
-               } else if (response.role === "Intern") {
-                    response2 = await inquirer.prompt([{
-                         type: "input",
-
-                         //the x is to only store into the team array
-                         name: "x",
-                         message: "What school is the employee attending?:",
-                    }, ]);
-                    //store the object and push
-                    const intern = new Intern(response.name, response.id, response.email, response2.x);
-                    teamArray.push(intern);
-               } else if (response.role === "Manager") {
-                    response2 = await inquirer.prompt([{
-                         type: "input",
-                         name: "x",
-                         message: "What is the employee's office number?:",
-                    }, ]);
-                    //store the object and push
-                    const manager = new Manager(response.name, response.id, response.email, response2.x);
-                    teamArray.push(manager);
-               }
-          } catch (err) {
-               return console.log(err);
-          }
-          console.log(teamArray)
-          //need to prompt do you want to continue
-
-          responseDone = await inquirer.prompt([{
-               type: "list",
-               name: "finish",
-               message: "Do you want to continue?: ",
-               choices: [
-                    "Yes",
-                    "No"
-               ]
-          }, ]);
-
-          console.log(responseDone.choices);
-          //the while parameter is saying continue running the code if the user selects "yes"
-     } while (responseDone.finish === "Yes");
+function newEmployee() {
+    inquirer.prompt([
+        {
+            type:"confirm",
+            name:"newEmployee",
+            message:"Would you like to enter another employee?",
+            default:true
+        }
+    ]).then(function(answers) {
+        if (answers.newEmployee) {
+            promptUser();
+        }
+        else {
+            const html = render(employeeList);
+            fs.writeFile(outputPath, html, function(err) {
+                if (err) throw err;
+            })
+        }
+    })
 }
 
+promptUser();
 
 
-
-
-
-
-//call function to run application on the server
-main();
-
-module.exports = teamstr
-`<div class="card">
-  <div class="card-header">
-      <h2>${arr.name}</h2>  
-      <h2>${arr.title}</h2>
-  </div>
- <div class="card-body">
-      <ul>
-        <li>${arr.id}</li>
-        <li>${arr.email}</li>
-        <li>${arr.x}</li>
-     </ul>
-  </div>
-  </div>`
-
-  `<div class="card">
-  <div class="card-header">
-     <h2>${arr.name}</h2>  
-      <h2>${arr.title}</h2>
-  </div>
-  <div class="card-body">
-      <ul>
-          <li>${arr.id}</li>
-          <li>${arr.email}</li>
-          <li>${arr.x}</li>
-      </ul>
-  </div>
-  </div>`
-
-  `<div class="card">
-    <div class="card-header">
-        <h2>${arr.name}</h2>  
-        <h2>${arr.title}</h2>
-    </div>
-    <div class="card-body">
-        <ul>
-            <li>${arr.id}</li>
-            <li>${arr.email}</li>
-           <li>${arr.x}</li>
-       </ul>
-    </div>
-    </div>`
- 
-  
