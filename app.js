173376b914734b673732b2f4bf84e84761594722
Hira Soomro <hira@Hiras-MacBook-Pro.local>
2023-03-20 21:14:52 -0700

    updated render and deleted extra files

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "./Output/teams.html");
-const render = require("lib/html.js");
+
+const render = require("./template/render")
 
 const employeeList = [];
 
@@ -99,7 +97,7 @@ function newEmployee() {
         }
         else {
             const html = render(employeeList);
-            fs.writeFile(outputPath, html, function(err) {
+            fs.writeFile("./Output/result.html", html, function(err) {
                 if (err) throw err;
             })
         }
