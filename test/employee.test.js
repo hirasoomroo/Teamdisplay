81474ab96d392e4cc97467da5fac684b91fa3232
Hira Soomro <hira@Hiras-MacBook-Pro.local>
2023-03-22 12:50:17 -0700

    added testing

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,7 +1,31 @@
-const { default: test } = require('node:test');
-const employee = require ('../lib/employee');
 
-test("Can Instantiate employee instance", () => {
-    
-}
-)
+const Employee = require('../lib/employee');
+
+describe('employee', () => {
+    it('should return an object', () => {
+        const person = new Employee("Alice")
+
+        
+    });
+
+    it('should return a name', () => {
+        const person = new Employee("Alice")
+
+        expect(person.name).toEqual("Alice");
+
+    });
+
+    it('should return a id', () => {
+        const person = new Employee("Alice", 23);
+
+        expect(person.id).toEqual(23);
+
+    });
+
+    it('should return a email', () => {
+        const person = new Employee("Alice", 23, "Alice@mail.com");
+
+        expect(person.email).toEqual("Alice@mail.com");
+
+    });
+});
